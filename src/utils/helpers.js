24f8519b44659caedf6322138758158b44a994eb
kdchurch1994 <kdchurch@bsu.edu>
2021-09-14 19:59:29 -0400

    created a function in the utils directory that will validate the email being used by the person filling out the contact form

---
+++ src/utils/helpers.js
@@ -0,0 +1,4 @@
+export function emailValidation(email) { //creates and exports the function that is being used to validate the email address that will be entered by the user when filling out the contact form. 
+    var regularExpression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regularExpression.test(String(email).toLowerCase());
+}
