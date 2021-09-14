5c8a892c6e0f6f398e79b29269060fa97aa93a20
kdchurch1994 <kdchurch@bsu.edu>
2021-09-14 04:49:57 -0400

    created NavComponent with links to about, portfolio, contact, and resume

---
+++ src/components/Nav/index.js
@@ -0,0 +1,26 @@
+import React from 'react'; //imports the use of react
+
+function Nav(props) { //function that creates the Nav component
+    const { currentLink, setCurrentLink } = props; //const that will be used to create the links to the various options in the Navbar
+
+    return (
+        <nav>
+            <ul>
+                <li className={currentLink === "about" ? "mx-2 navActive": "mx-2"}>
+                    <span onClick={() => setCurrentLink('about')}>About Me</span>
+                </li>
+                <li className={currentLink === "portfolio" ? "mx-2 navActive": "mx-2"}>
+                    <span onClick={() => setCurrentLink('portfolio')}>Portfolio</span>
+                </li>
+                <li className={currentLink === "contact" ? "mx-2 navActive": "mx-2"}>
+                    <span onClick={() => setCurrentLink('contact')}>About Me</span>
+                </li>
+                <li className={currentLink === "resume" ? "mx-2 navActive": "mx-2"}>
+                    <span onClick={() => setCurrentLink('resume')}>resume</span>
+                </li>
+            </ul>
+        </nav>
+    );   
+}
+
+export default Nav;
