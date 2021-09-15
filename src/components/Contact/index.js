ede99a99072f4eb8d1a22dce2f90dbf34b9eff3e
kdchurch1994 <kdchurch@bsu.edu>
2021-09-14 20:13:41 -0400

    created a contact form function

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import { emailValidation } from '../../utils/helpers';
+
+function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log('Submit Form', formState);
+        }
+    };
+}
